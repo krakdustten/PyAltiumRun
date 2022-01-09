7b1eee7cc15b12a0e11be4e9eb440cc690096cb3
DylanGybels <dylan.gybels@magics.tech>
2022-01-09 10:29:04 +0100

    Moved long description to CFG

--- setup.py
+++ setup.py
@@ -1,10 +1,4 @@
-from pathlib import Path
 from setuptools import setup
 
 if __name__ == "__main__":
-    this_directory = Path(__file__).parent
-    long_description = (this_directory / "README.md").read_text()
-    setup(
-        long_description=long_description,
-        long_description_content_type='text/markdown'
-    )
+    setup()
