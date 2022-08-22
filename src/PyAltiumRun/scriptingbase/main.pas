96b47ccf13db1594a0fd46b235cc0e8eddabdcf7
Mark Norman <mnorman@micro-x.com>
2022-08-22 21:23:11 +0930

    Updated logging function to match code in logger.pas.  Fixes #1

--- src/PyAltiumRun/scriptingbase/main.pas
+++ src/PyAltiumRun/scriptingbase/main.pas
@@ -7,10 +7,10 @@ Procedure SCRIPTING_SYSTEM_MAIN;
 Var
   Document: IDocument;
 Begin
-  write_str_to_log('Starting script');
+  log_str('Starting script');
   If AnsiCompareStr(ProjectFilePath, 'None') then
   Begin
-    write_str_to_log('Opening project: ' + ProjectFilePath);
+    log_str('Opening project: ' + ProjectFilePath);
     Document := Client.OpenDocument('PcbProject', ProjectFilePath);
     Client.ShowDocument(Document);
   End;
